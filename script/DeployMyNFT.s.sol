affe8c8ec0ce235e9e65f68621e03dc2c3058d29
0xNicos <psalmsprint@gmail.com>
2025-10-03 16:05:33 +0100

    Fix HelperConfig for CI and update tests

--- script/DeployMyNFT.s.sol
+++ script/DeployMyNFT.s.sol
@@ -19,6 +19,10 @@ contract DeployGenesis721 is Script {
         HelperConfig helper = new HelperConfig();
         (uint256 maxSupply, uint256 deployerKey) = helper.activeNetworkConfig();
 
+        if (deployerKey == 0x123) {
+            deployerKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        }
+
         vm.startBroadcast(deployerKey);
         Genesis721 genesis = new Genesis721(maxSupply);
         vm.stopBroadcast();
